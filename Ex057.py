c2e166e48e336fb37e519a962529571e1e9f1330
Fernando <fernando22future@gmail.com>
2021-02-27 15:59:09 -0300

    Resolução incompleta do Ex057

--- Ex057.py
+++ Ex057.py
@@ -1,3 +1,9 @@
 """Faça um programa que leia o sexo de uma pessoa, mas só aceite os valore 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto."""
 
+sexo = input('Digite o sexo de uma pessoa [M/F]: ').upper()
 
+while sexo != 'M':
+    sexo = input('Digite corretamente o sexo da pessoa!!! Digite o sexo de uma pessoa [M/F]: ').upper()
+while sexo != 'F':
+    sexo = input('Digite corretamente o sexo da pessoa!!! Digite o sexo de uma pessoa [M/F]: ').upper()
+print('Você digitou corretamente o sexo!')
