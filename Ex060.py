97abf96a148e7a566d74de8223778663480fb5fd
Fernando <fernando22future@gmail.com>
2021-03-02 16:04:14 -0300

    Resolução do Ex060

--- Ex060.py
+++ Ex060.py
@@ -1,3 +1,11 @@
 """Faça um programa que leia um número qualquer e mostre o seu fatorial. """
 
+numero = int(input('Digite um valor para ser calculado o fatorial '))
+n = 1
+fatorial = 1
 
+while n < numero + 1:
+    fatorial = fatorial * n
+    n += 1
+
+print('O fatorial do número {} é {}'.format(numero, fatorial))
