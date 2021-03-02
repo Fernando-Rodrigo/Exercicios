e63736bde6416c399a06e43e322ad664cdd8c9c1
Fernando <fernando22future@gmail.com>
2021-03-02 16:29:53 -0300

    Resolução do Ex061

--- Ex061.py
+++ Ex061.py
@@ -1,3 +1,13 @@
 """Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da prograssão usando a estrutura while."""
 
+a1 = int(input('Digite o primeiro termo da progreção aritmética: '))
+s = int(input('Digite a razão da progreção: '))
 
+pa = a1
+
+c = 0
+
+while c < 10:
+    pa += s
+    print('O termo {} da PA é {}'.format(c + 1, pa))
+    c += 1
