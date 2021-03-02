65500494eeb284fbf50872164b080eda8a40353b
Fernando <fernando22future@gmail.com>
2021-03-02 20:48:34 -0300

    Resolução do Ex063

--- Ex063.py
+++ Ex063.py
@@ -1,3 +1,14 @@
 """Escreva um programa que leia um númer n inteiro qualquer e mostre na tela os n primeiros elementos da sequência de Fibonacci. """
 
+n = int(input('Digite um valor de n termos da sequência de Fibonacci: '))
+fibo = 1
+c = 1
+temp1 = 1
+temp2 = 1
 
+while c < n:
+    fibo = temp1 + temp2
+    temp2 = temp1
+    temp1 = fibo
+    print('{}'.format(fibo))
+    c += 1
