66531807bad160a78cf4d6faac22207a27ac5048
Fernando <fernando22future@gmail.com>
2021-03-02 15:25:19 -0300

    Resolução do Ex058

--- Ex058.py
+++ Ex058.py
@@ -1,3 +1,13 @@
 """Melhore o jogo do desafio 28 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer."""
 
+from random import randint
 
+n = int(input('Tente adivinhar um número entre 0 e 10 '))
+r = int(randint(0, 10))
+
+while n != r:
+    n = int(input('Tente adivinhar um número entre 0 e 10 '))
+    if n == r:
+        print('Parabéns, você acertou o número!!!')
+    else:
+        print('Não foi desta vez. Tente outra vez.')
