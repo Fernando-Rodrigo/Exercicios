c97728815d9ca1c131a84efeecf2f81a19903d95
Fernando <fernando22future@gmail.com>
2021-03-02 21:32:03 -0300

    Resolução do Ex065

--- Ex065.py
+++ Ex065.py
@@ -1,3 +1,19 @@
 """Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média de todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores."""
 
+num = int(input('Digite um número: '))
 
+maior = menor = num
+
+cont = 's'
+soma = 0
+
+while cont != 'n':
+    if num > maior:
+        maior = num
+    if num < menor:
+        menor = num
+    soma += num
+    num = int(input('Digite um novo número: '))
+    cont = str(input('Deseja continuar? [s/n]')).lower()
+
+print('O maior número digitado é {},\no menor número digitado é {}\ne a soma entre eles é {}.'.format(maior, menor, soma))
