ff78f67a820f02d83bb8d34875c9adcb10532b26
Fernando <fernando22future@gmail.com>
2021-03-03 15:13:55 -0300

    Atualização do Ex059

--- Ex059.py
+++ Ex059.py
@@ -5,17 +5,17 @@ n2 = float(input('Digite o segundo valor: '))
 
 opcao = int(input('Qual a opção de operação que deseja realizar: \n[1]Somar\n[2]Multiplicar\n[3]Maior\n[4]Novos números\n[5]Sair do programa. '))
 
-while opcao !=5:
+while opcao != 5:
     if opcao == 1:
         print('A soma dos valores {} com {} é {}'.format(n1, n2, n1 + n2))
-    if opcao == 2:
+    elif opcao == 2:
         print('O resultado da multiplicação entre {} e {} é {}'.format(n1, n2, n1 * n2))
-    if opcao == 3:
+    elif opcao == 3:
         if n1 > n2:
             print('O maior número digitado é {}'.format(n1))
         else:
             print('O maior número digitado é {}'.format(n2))
-    if opcao == 4:
+    elif opcao == 4:
         n1 = float(input('Digite o primeiro valor novo: '))
         n2 = float(input('Digite o segundo valor novo: '))
     opcao = int(input('Qual a opção de operação que deseja realizar: \n[1]Somar\n[2]Multiplicar\n[3]Maior\n[4]Novos números\n[5]Sair do programa. '))
