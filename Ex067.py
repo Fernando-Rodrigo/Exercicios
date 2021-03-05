05daf6b5efaa47e0d34bb4cc0f9b26f955383949
Fernando <fernando22future@gmail.com>
2021-03-05 16:58:31 -0300

    Resolução do Ex67

--- Ex067.py
+++ Ex067.py
@@ -1,3 +1,16 @@
 """Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O Programa será interrompido quando o número solicitado for negativo."""
 
+cont = 1
 
+while True:
+    n = int(input('Digite um número:'))
+    print('---' * 10)
+    
+    if n < 0:
+        break
+    else:
+        for cont in range (1, 11):
+            print(f'{n} * {cont} = {(cont * n)}')
+            cont += 1
+    
+    print('---' * 10)
