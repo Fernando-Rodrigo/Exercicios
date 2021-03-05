5668708c4cc8c1850479b3dc5645e92803b1a1f8
Fernando <fernando22future@gmail.com>
2021-03-05 15:42:58 -0300

    Resolução do Ex66

--- Ex066.py
+++ Ex066.py
@@ -1,3 +1,15 @@
 """Crie um programa que leia vários números inteiros pelo teclado. O programa vai parar quando o usuário digitar 999, que é a condição de parada. No final mostre quantos números foram digitados e qual foi a soma entre eles(desconsiderando a flag)."""
 
+n = soma = 0
 
+cont = 0
+
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    else:
+        soma += n
+        cont += 1
+
+print(f'Foram digitados {cont} e a soma entre eles é {soma}.')
