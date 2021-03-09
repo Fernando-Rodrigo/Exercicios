a2a8d778b093c4316c96e86b83e83197ef31b8cf
Fernando <fernando22future@gmail.com>
2021-03-09 19:30:22 -0300

    Resolução do Ex071

--- Ex071.py
+++ Ex071.py
@@ -1,3 +1,21 @@
 """Crie um programa que simule um caixa eletrônico. No início, pergunte ao usuário qual será o valor sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. Obs: considere que o caixa tenha notas de R$50, R$20, R$10 e R$1."""
 
+from math import floor
 
+valor = int(input('Qual é o valor que deseja sacar? R$'))
+
+valor50 = valor20 = valor10 = valor1 = resto = 0
+
+while True:
+    valor50 = floor(valor / 50)
+    resto = valor % 50
+    valor20 = floor(resto / 20)
+    resto = resto % 20
+    valor10 = floor(resto / 10)
+    valor1 = resto % 10
+    break
+
+print(f'A quantidade de notas de R$50,00 é {valor50}.')
+print(f'A quantidade de notas de R$20,00 é {valor20}.')
+print(f'A quantidade de notas de R$10,00 é {valor10}.')
+print(f'A quantidade de notas de R$1,00 é {valor1}.')
