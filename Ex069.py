1c0291488723e483dc3492fb897fa48374c52728
Fernando <fernando22future@gmail.com>
2021-03-10 19:07:57 -0300

    Atualização do Ex069

--- Ex069.py
+++ Ex069.py
@@ -3,14 +3,14 @@
 Cont18 = ContHomens = Cont20 = 0
 
 while True:
-    sexo = str(input('Qual é o sexo da pessoa? [M/F] ')).upper().strip()[0]
-    while sexo != 'M' and sexo != 'F':
+    sexo = ' '
+    while sexo not in 'MF':
         sexo = str(input('Qual é o sexo da pessoa? [M/F] ')).upper().strip()[0]
     idade = int(input('Qual é a idade da pessoa cadastrada? '))
     while idade < 0:
           idade = int(input('Qual é a idade da pessoa cadastrada? '))
-    cont = str(input('Deseja continuar a cadastrar? [S/N] ')).upper().strip()[0]
-    while cont != 'S' and cont != 'N':
+    cont = ' '
+    while cont not in 'NS':
         cont = str(input('Deseja continuar a cadastrar? [S/N] ')).upper().strip()[0]
     if idade >= 18:
         Cont18 += 1
