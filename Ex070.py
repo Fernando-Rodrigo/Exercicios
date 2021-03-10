418c66fdefd46f6459f4d2670a376310e6c94698
Fernando <fernando22future@gmail.com>
2021-03-10 19:35:08 -0300

    Atualização do EX070

--- Ex070.py
+++ Ex070.py
@@ -1,29 +1,28 @@
 """Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar. No final, mostre: A) Qual é o total gasto na compra. B) Quantos produtos custam mais de R$1000,00. C) Qual o nome do produto mais barato."""
 
-total = quant1000 = menorpreco = 0
-nomem = ''
+total = quant1000 = menorpreco = c = 0
+nomem = ' '
 
 while True:
     nome = str(input('Qual é o nome do produto? '))
     preco = float(input('Qual é o preço do produto? R$'))
+    c += 1
     
-    cont = str(input('Deseja continuar cadastrando produtos? [S/N] ')).upper().strip()[0]
+    cont = ' '
     
-    while cont != 'S' and cont != 'N':
+    while cont not in 'SN':
         cont = str(input('Deseja continuar cadastrando produtos? [S/N] ')).upper().strip()[0]
     
     total += preco
-    menorpreco = preco
-    nomem = nome
 
     if preco > 1000:
         quant1000 += 1
-    if preco < menorpreco:
-        nomem = nome
+    if c == 1 or preco < menorpreco:
         menorpreco = preco
+        nomem = nome
     if cont == 'N':
         break
 
-print(f'O valor total da compra é de R${total}.')
-print(f'Foram cadastrados {quant1000} produtos que custam mais de R$1000,00.')
-print(f'O produto mais barato é {nomem} que custa R${menorpreco :.2f}.')
+print(f'O valor total da compra é de R${total :.2f}')
+print(f'Foram cadastrados {quant1000} produtos que custam mais de R$1000.00')
+print(f'O produto mais barato é {nomem} que custa R${menorpreco :.2f}')
