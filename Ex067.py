d756981c605625f3fd52d48b53f15e8543bb223c
Fernando <fernando22future@gmail.com>
2021-03-10 17:35:21 -0300

    Atualização do EX067

--- Ex067.py
+++ Ex067.py
@@ -1,7 +1,7 @@
 """Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O Programa será interrompido quando o número solicitado for negativo."""
 
 while True:
-    n = int(input('Digite um número:'))
+    n = int(input('Digite um número: '))
     print('---' * 10)
     
     if n < 0:
