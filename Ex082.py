f1ea68f88809377db5bd12aed3427a7d936ebc47
Fernando <fernando22future@gmail.com>
2021-05-18 16:23:10 -0300

    Resolução dos Exercícios 082 e 083
    O exercício 083 está errado e ainda não sei como solucioná-lo.

--- Ex082.py
+++ Ex082.py
@@ -1,3 +1,21 @@
 """Crie um programa que vai ler vários números e colocar em uma lista. Depois disso crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas."""
 
+valores = []
+pares = []
+impares = []
+continuar = ''
 
+while True:
+    valores.append(int(input('Entre um valor: ')))
+    continuar = str(input('Deseja continuar? [s/n] '))
+    if continuar == 'n':
+        break
+
+for i in range (0, len(valores)):
+    if valores[i] % 2 == 0:
+        pares.append(valores[i])
+    elif valores[i] % 2 != 0:
+        impares.append(valores[i])
+
+print(f'Os valores pares digitados foram {pares}')
+print(f'Os valores impares digitados foram {impares}')
--- Ex083.py
+++ Ex083.py
@@ -1,3 +1,17 @@
 """Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deve analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta."""
 
+expressao = []
 
+count1 = count2 = 0
+
+expressaostr = str(input('Digite a expressão matemática: ')).split()
+
+# expressao.append(input('Digite a expressão matemática: '))
+
+for i in range (0, len(expressaostr)):
+    if expressaostr[i] == '(':
+        count1 += 1
+    elif expressaostr[i] == ')':
+        count2 += 1
+
+print(f'{count1}')
