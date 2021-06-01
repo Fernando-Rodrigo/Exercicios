1b38e1bcdd69ea250f87b679afba6e5a92ef0656
Fernando <fernando22future@gmail.com>
2021-06-01 15:54:13 -0300

    Correção do ex 078

--- Ex078.py
+++ Ex078.py
@@ -5,5 +5,14 @@ valores = []
 for i in range (0,5):
     valores.append(int(input(f'Digite o {i + 1}º valor: ')))
 
-print(f'O maior valor da lista é {max(valores)} e está na posição {valores.index(max(valores)) + 1} da lista.')
-print(f'O menor valor da lista é {min(valores)} e está na posição {valores.index(min(valores)) + 1} da lista.')
+print(f'O maior valor da lista é {max(valores)} e está nas posições ', end = '')
+for i,v in enumerate(valores):
+    if v == max(valores):
+        print(f'{i} ', end = '')
+print()
+
+print(f'O menor valor da lista é {min(valores)} e está nas posições ', end = '')
+for i,v in enumerate(valores):
+    if v == min(valores):
+        print(f'{i} ', end = '')
+print()
