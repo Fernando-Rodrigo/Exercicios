74373f3ddb943835dc3d777db2bbcff815fe7c70
Fernando <fernando22future@gmail.com>
2021-06-02 16:38:26 -0300

    Correção do exercício 080

--- Ex080.py
+++ Ex080.py
@@ -1,16 +1,15 @@
 """Crie um programa onde o usuário possa digitar 5 valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem o sort()). No final mostre a lista ordenanda na tela."""
 
 valores = []
-num = 0
 
 for i in range(0, 5):
     num = int(input('Digite um valor: '))
-    if i == 0:
+    if i == 0 or num > valores[-1]:
         valores.append(num)
     else:
-        if num < valores[i - 1]:
-            valores.insert(i - 1, num)
-        else:
-            valores.append(num)
+        for pos in range(0, 5):
+            if num <= valores[pos]:
+                valores.insert(pos, num)
+                break
 
-print(valores)
+print(f'Os valores digitados foram {valores}')
