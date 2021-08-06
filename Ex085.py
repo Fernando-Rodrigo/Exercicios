d746fa9a18cfdc9ae6c50096c1283c9a69181946
Fernando <fernando22future@gmail.com>
2021-08-06 14:09:10 -0300

    Update do exercício 085

--- Ex085.py
+++ Ex085.py
@@ -1,19 +1,16 @@
 """Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e impares. No final mostre os valores pares e impares em ordem crescente."""
 
-impares = []
-pares = []
-valores = [impares, pares]
+valores = [[], []]
 
 for i in range(0,7):
-    num = int(input(f'Digite o {i + 1}° número: '))
+    num = int(input(f'Digite o {i + 1}º número: '))
     if num % 2 == 0:
-        pares.append(num)
+        valores[0].append(num)
     elif num % 2 != 0:
-        impares.append(num)
+        valores[1].append(num)
 
-pares.sort()
-impares.sort()
+valores[0].sort()
+valores[1].sort()
 
-print(f'Os números pares digitados são {pares}')
-print(f'Os números impares digitados são {impares}')
-print(f'A lista completa com os núeros pares digitdos é {valores[1]} e impares é {valores[0]}')
+print(f'Os números pares digitados são {valores[0]}')
+print(f'Os números impares digitados são {valores[1]}')
