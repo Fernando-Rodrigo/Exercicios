89c0e14ade92c69fb59e10907c2d7fd1a9307d2a
Fernando <fernando22future@gmail.com>
2021-08-06 17:23:52 -0300

    Resolução do exercício 089

--- Ex089.py
+++ Ex089.py
@@ -1,3 +1,33 @@
 """Crie um programa que leia o nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário mostre as notas de cada aluno individualmente."""
 
+notas = [[], [], []]
+sair = aluno = opcao = ''
+indice = 0
 
+
+while True:
+    notas[0].append(str(input('Digite o nome do aluno ')))
+    notas[1].append(float(input('Digite a primeira nota: ')))
+    notas[2].append(float(input('Digite a segunda nota: ')))
+    sair = str(input('Deseja sair? [s/n] ')).lower().strip()
+    if sair == 's':
+        break
+
+for c in range(0,len(notas[0])):
+    print(f'A média do aluno {notas[0][c]} é {(notas[1][c] + notas[2][c]) / 2}')
+
+opcao = str(input('Deseja ver as notas do aluno? [s/n] ')).lower().strip()
+if opcao == 's':
+    while True:
+        aluno = str(input('Qual é o nome do aluno que deseja ver as notas? '))
+        if aluno in notas[0]:
+            indice = notas[0].index(aluno)
+        else:
+            print('Aluno não encontrado na lista!!')
+        print(f'As notas do {aluno} são {notas[1][indice]} e {notas[2][indice]}.')
+        sair = str(input('Deseja consultar mais notas? [s/n] ')).lower().strip()
+        if sair == 'n':
+            break
+        print('Obrigado pela presença!!')
+else:
+    print('Obrigado pela presença!!')
