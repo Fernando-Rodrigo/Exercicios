2078916f3fd143e2848fa548d6b231a8954aa101
Fernando <fernando22future@gmail.com>
2021-08-07 17:07:46 -0300

    Resolução do exercício 090

--- Ex090.py
+++ Ex090.py
@@ -1,3 +1,15 @@
-"""Faça um programa que leia o nome e média de um aluno, guardanto também a situação em um dicionário. No final mostre, mostre o conteúdo da estrutura na tela. Fazer para um único aluno e usar a nota 7 como referência mínima para aprovção."""
+"""Faça um programa que leia o nome e média de um aluno, guardando também a situação em um dicionário. No final mostre, mostre o conteúdo da estrutura na tela. Fazer para um único aluno e usar a nota 7 como referência mínima para aprovção."""
 
+aluno = {}
 
+aluno['nome'] = str(input('Digite o nome do aluno: '))
+aluno['media'] = float(input('Digite a média do aluno: '))
+
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif aluno['media'] >= 5:
+    aluno['situacao'] = 'Recupeação'
+elif aluno['media'] < 5:
+    aluno['situacao'] = 'Reprovado'
+
+print(f'O aluno {aluno["nome"]} teve de média {aluno["media"]} e a situação é {aluno["situacao"]}.')
