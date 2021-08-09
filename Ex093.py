91b1d163f56be40c10dcf72c2553583400b22f96
Fernando <fernando22future@gmail.com>
2021-08-09 15:12:28 -0300

    Resolução do exercício 093

--- Ex093.py
+++ Ex093.py
@@ -1,3 +1,20 @@
-"""Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quants partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será gardado em um dicionário, incluindo o total de gols no campeonato. Fazer isso para um jogador apenas."""
+"""Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será gardado em um dicionário, incluindo o total de gols no campeonato. Fazer isso para um jogador apenas."""
 
+jogador = {}
+totalgols = 0
 
+jogador['nome'] = str(input('Digite o nome do jodador: '))
+jogador['partidas'] = int(input('Digite quantas partidas o jogador participou: '))
+
+for i in range(0,jogador["partidas"]):
+    jogador[f'gols{i}'] = int(input(f'Digite gols fez na partida {i + 1}: '))
+    totalgols += jogador[f'gols{i}']
+
+jogador['total'] = totalgols
+
+print()
+print('--' * 30)
+print(f'O jogador {jogador["nome"]} participou de {jogador["partidas"]} partidas.')
+for i in range(0,jogador["partidas"]):
+    print(f'Na partida {i + 1} o jodador fez {jogador[f"gols{i}"]} gols')
+print(f'O saldo de gols da temporada é {jogador["total"]}')
