183c3694930b4c5eec43e76180f35dbcbcd8aebf
Fernando <fernando22future@gmail.com>
2021-08-09 16:08:57 -0300

    Atualização do exercício 093

--- Ex093.py
+++ Ex093.py
@@ -1,20 +1,29 @@
 """Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será gardado em um dicionário, incluindo o total de gols no campeonato. Fazer isso para um jogador apenas."""
 
 jogador = {}
-totalgols = 0
+temp = []
 
 jogador['nome'] = str(input('Digite o nome do jodador: '))
-jogador['partidas'] = int(input('Digite quantas partidas o jogador participou: '))
+partidas = int(input('Digite quantas partidas o jogador participou: '))
 
-for i in range(0,jogador["partidas"]):
-    jogador[f'gols{i}'] = int(input(f'Digite gols fez na partida {i + 1}: '))
-    totalgols += jogador[f'gols{i}']
+for c in range(0, partidas):
+    temp.append(int(input(f'Digite gols fez na partida {c + 1}: ')))
 
-jogador['total'] = totalgols
+jogador['gols'] = temp[:]
+jogador['total'] = sum(temp)
 
 print()
 print('--' * 30)
-print(f'O jogador {jogador["nome"]} participou de {jogador["partidas"]} partidas.')
-for i in range(0,jogador["partidas"]):
-    print(f'Na partida {i + 1} o jodador fez {jogador[f"gols{i}"]} gols')
+print(jogador)
+
+print()
+print('--' * 30)
+for k,v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+
+print()
+print('--' * 30)
+print(f'O jogador {jogador["nome"]} participou de {len(jogador["gols"])} partidas.')
+for i, v in enumerate(jogador["gols"]):
+    print(f'Na partida {i + 1} o jodador fez {v} gols')
 print(f'O saldo de gols da temporada é {jogador["total"]}')
