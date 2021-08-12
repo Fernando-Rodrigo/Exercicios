699c3c5bd19f6f59f330c7f3c03c247281b836af
Fernando <fernando22future@gmail.com>
2021-08-12 14:38:36 -0300

    Resolução do exercício 097

--- Ex097.py
+++ Ex097.py
@@ -1,3 +1,13 @@
 """Faça um programa que tenha a função escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com com tamanho adaptável. Ex: escreva('olá, mundo!') saída: ----------- Olá, mundo! -----------"""
 
+def escreva(frase):
+    print('--' * len(frase))
+    print(' ' * ((len(frase)//2) - 1), frase)
+    print('--' * len(frase))
 
+
+escreva('casarão')
+escreva('casa')
+escreva('escola')
+escreva('Curso em vídeo')
+escreva('Super casa automática')
