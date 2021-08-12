59eb0c2ab6ce7cd3dccaa5b16047a65314d0d093
Fernando <fernando22future@gmail.com>
2021-08-12 14:22:38 -0300

    Resolução do exercício 096

--- Ex096.py
+++ Ex096.py
@@ -1,3 +1,13 @@
 """Faça um programa que tenha uma função chamada area(), que receba as dimensões de um terreno retangular(largura e comprimento) e mostre a área do terreno."""
 
+a = 0
 
+def area(l, c):
+    a = l * c
+    print()
+    print(f'A área do retângulo é de {a}m².')
+
+
+largura = int(input('Digite a largura de um retângulo(m): '))
+comprimento = int(input('Digite o comprimento do retângulo(m): '))
+area(largura, comprimento)
