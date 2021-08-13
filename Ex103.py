8513f2733d8c78e4c62d158fd0fe652c4b6b2893
Fernando <fernando22future@gmail.com>
2021-08-13 20:29:08 -0300

    Resolução do exercício 103

--- Ex103.py
+++ Ex103.py
@@ -1,3 +1,9 @@
-"""Faça um programa qe tenha uma função chamada ficha(), que recebe dois parâmetros opcionais: o nome do jogador(padrã= <desconhecido>) e quantos gols ele marcou(padrão=0 gol(s) marcados). O programa devera ser capaz de mostrar a ficha do jogador, mesmo que algum dado nãotenha sido informado corretamente."""
+"""Faça um programa qe tenha uma função chamada ficha(), que recebe dois parâmetros opcionais: o nome do jogador(padrão= <desconhecido>) e quantos gols ele marcou(padrão=0 gol(s) marcados). O programa devera ser capaz de mostrar a ficha do jogador, mesmo que algum dado nãotenha sido informado corretamente."""
 
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} no campeonato.')
 
+nome = str(input('Qual é o nome do jogador? '))
+gols = int(input('Quantos gols o jogador fez na temporada? '))
+
+ficha(nome, gols)
