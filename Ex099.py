b05804797f262d61bce80c36b5183a9ca2d7252d
Fernando <fernando22future@gmail.com>
2021-08-12 21:43:13 -0300

    Atualização do exercício 099

--- Ex099.py
+++ Ex099.py
@@ -1,9 +1,6 @@
 """Faça um programa que tenha uma função chamada maior(), que receba vários parãmetros com valores inteiros. Seu programa deve anlisar todos os valores e dizer qual deles é o maior. Informar a quantidade de números passados como parâmetros."""
 
-valores = []
-cont = ''
-
-def maior(v):
+def maior(* v):
     print()
     print('--' * 30)
     print(f'Os valores digitados foram {v}')
@@ -12,14 +9,6 @@ def maior(v):
     print('--' * 30)
 
 
-while True:
-    valores.append(int(input('Digite um valor: ')))
-    while True:
-        cont = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
-        if cont in 'SsNn':
-            break
-        print('Escolha somente sim ou não')
-    if cont == 'N':
-        break
-
-maior(valores)
+maior(0, 9, 7, 3, 5)
+maior(6)
+maior(99, 104, 3, 200, 1, 25, 73, 88, 99, 111)
