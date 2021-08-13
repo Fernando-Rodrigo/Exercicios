6b1199cda15576404a649c6b0aee762eeef63359
Fernando <fernando22future@gmail.com>
2021-08-13 19:20:32 -0300

    Resolução do exercício 101

--- Ex101.py
+++ Ex101.py
@@ -1,3 +1,28 @@
 """Crie um programa que tenha uma função chamada voto(), que vai receber como parâmetro, o ano de nascimento de uma pessoa(calcular idade), retornando um valor literal(string), indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições."""
 
+from datetime import datetime
 
+def voto(ano):
+    """
+    ->Calcula a idade e diz se é não é preciso votar, se é opcional, ou se é obrigatório votar.
+    :param ano: ano de nascimento da pessoa
+    :return: 'NEGADO' ou 'NEGADO' ou 'OBRIGATÓRIO'
+    """
+    idade = datetime.now().year - ano
+    if idade < 16:
+        return 'NEGADO'
+    elif idade < 18:
+        return 'NEGADO'
+    elif idade < 65:
+        return 'OBRIGATÓRIO'
+    else:
+        return 'OPCIONAL'
+
+
+#programaprincipal
+ano = int(input('Qual é o ano de nascimento? '))
+
+print()
+print('--' * 30)
+print(f'A idade é {datetime.now().year - ano} ano(s) e o voto é {voto(ano)}')
+print('--' * 30)
