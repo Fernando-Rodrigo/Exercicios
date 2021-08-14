b455c9a6530fab14b8dab99b2d3ad3275319868b
Fernando <fernando22future@gmail.com>
2021-08-13 23:20:36 -0300

    Resolução do exercício 106

--- Ex106.py
+++ Ex106.py
@@ -1,3 +1,12 @@
-"""Faça um mini-programa que utilize o INteractive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. OBS: use cores."""
+"""Faça um mini-programa que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. OBS: use cores."""
 
 
+def ajuda(comando):
+    help(comando)
+
+
+while True:
+    cmd = str(input('\33[30;46mQual é o comando que deseja ver o manual? (FIM para encerrar) '))
+    if cmd in 'FIMFimfim':
+        break
+    ajuda(cmd)
