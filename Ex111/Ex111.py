3038b761eeb3c9e31bd7eb7198bef683a79aafd2
Fernando <fernando22future@gmail.com>
2021-08-16 19:20:22 -0300

    Resolução do exercício 111

--- Ex111/Ex111.py
+++ Ex111/Ex111.py
@@ -1,3 +1,6 @@
 """Crie um pacote chamado utilidadeCeV que tenha dois módulos internos chamados moeda e dado. Transfira todas as funções utilizadas nos desafios 107, 108, 109 para o primeiro pacote e mantenha tudo funcionando."""
 
+from utilidadeCeV import moeda
 
+valor = float(input('Digite um valor monetário: R$'))
+print(moeda.resumo(valor))
