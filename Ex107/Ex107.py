4a60661803fb751d148b0e8ffcef8c178bbfd296
Fernando <fernando22future@gmail.com>
2021-08-16 16:39:17 -0300

    Resolução do exercício 107

--- Ex107/Ex107.py
+++ Ex107/Ex107.py
@@ -1,3 +1,10 @@
 """Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro(), metade(). Faça um programa que importe esse modulo e use alguma dessas funções."""
 
+import moeda
 
+valor = float(input('Digite um valor monetário: R$'))
+
+print(f'O valor R${valor} com 20% de aumento é R${moeda.aumentar(valor)}')
+print(f'O valor R${valor} com 30% de desconto é R${moeda.diminuir(valor)}')
+print(f'O dobro do valor R${valor} é R${moeda.dobro(valor)}')
+print(f'A metade do valor R${valor} é R${moeda.metade(valor)}')
---
+++ Ex107/moeda.py
@@ -0,0 +1,14 @@
+def aumentar(valor):
+    return valor + valor * 0.2
+
+
+def diminuir(valor):
+    return valor - valor * 0.3
+
+
+def dobro(valor):
+    return valor * 2
+
+
+def metade(valor):
+    return valor / 2
