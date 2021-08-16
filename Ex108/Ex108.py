f9ae41949d4ab60ec4728b368c73edd19c632155
Fernando <fernando22future@gmail.com>
2021-08-16 17:14:49 -0300

    Resolução do exercício 108

--- Ex108/Ex108.py
+++ Ex108/Ex108.py
@@ -1,3 +1,10 @@
 """Adapte o exercício 107, criando uma função adicional chamada moeda() que consiga mostrar os valores como valor monetário formatado."""
 
+import moeda
 
+valor = float(input('Digite um valor monetário: R$'))
+
+print(f'O valor R${moeda.moeda(valor)} com 20% de aumento é R${moeda.moeda(moeda.aumentar(valor))}')
+print(f'O valor R${moeda.moeda(valor)} com 30% de desconto é R${moeda.moeda(moeda.diminuir(valor))}')
+print(f'O dobro do valor R${moeda.moeda(valor)} é R${moeda.moeda(moeda.dobro(valor))}')
+print(f'A metade do valor R${moeda.moeda(valor)} é R${moeda.moeda(moeda.metade(valor))}')
---
+++ Ex108/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(valor):
+    return valor + valor * 0.2
+
+
+def diminuir(valor):
+    return valor - valor * 0.3
+
+
+def dobro(valor):
+    return valor * 2
+
+
+def metade(valor):
+    return valor / 2
+
+
+def moeda(valor):
+    return f'{valor :.2f}'
