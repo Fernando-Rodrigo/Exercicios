325df83ef52ad74cf7c3ccac5274cf3d738f2c58
Fernando <fernando22future@gmail.com>
2021-08-17 15:49:07 -0300

    Correção do exercício 112

--- Ex112/Ex112.py
+++ Ex112/Ex112.py
@@ -1,8 +1,8 @@
 """Dentro do pacote utilidadeCeV que criamos no desafio 111, temos o módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como função input(), mas com a validação de dados, para aceitar apenas valores que sejam monetários."""
 
-import utilidadeCeV
+from utilidadeCeV import dado, moeda
 
-valor = utilidadeCeV.dado.leiaDinheiro('Digite um valor monetário: R$')
-taxa = float(input('Digite a taxa: '))
+valor = dado.leiaDinheiro('Digite um valor monetário: R$')
+taxa = dado.leiaDinheiro('Digite a taxa: ')
 
-print(utilidadeCeV.moeda.resumo(valor, taxa))
+moeda.resumo(valor, taxa)
--- Ex112/utilidadeCeV/moeda/__init__.py
+++ Ex112/utilidadeCeV/moeda/__init__.py
@@ -1,5 +1,14 @@
 def resumo(valor, taxa):
-    return f'\nO valor inserido é {moeda(valor)}\nCom aumento de {taxa}% fica {aumentar(valor, taxa)}\nCom {taxa}% desconto fica {diminuir(valor, taxa)}\nO dobro é {dobro(valor)}\nA metade é {metade(valor)}'
+    """
+    _>Função que recebe o valor monetário e a taxa para cálculo de desconto, aumento, metade e dobro do valor.
+    :param valor: Valor monetário
+    :param taxa: Valor da taxa para cálculo do desconto e aumento
+    """
+    print(f'O valor inserido é {moeda(valor)}')
+    print(f'Com aumento de {taxa :.2f}% fica {aumentar(valor, taxa)}')
+    print(f'Com {taxa :.2f}% desconto fica {diminuir(valor, taxa)}')
+    print(f'O dobro é {dobro(valor)}')
+    print(f'A metade é {metade(valor)}')
 
 
 def aumentar(valor, taxa):
