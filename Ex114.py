75ecf7c559bd2b15e4f7b1fb0d20cf984d0fe5fe
Fernando <fernando22future@gmail.com>
2021-08-18 21:32:11 -0300

    Atualização dos exercícios 114 e 115

--- Ex114.py
+++ Ex114.py
@@ -1,18 +1,12 @@
 """Crie um código em Python que teste se o site Pudim está acessível pelo computador usado."""
 
 from http.client import HTTPConnection
-from sys import stdout
 from ctypes import windll
-
-#cortexto = []
 
 def cor(texto, cornum):
     std_out_handle = windll.kernel32.GetStdHandle(-11)
     windll.kernel32.SetConsoleTextAttribute(std_out_handle, cornum)
-    for i in range(0, 1):
-        windll.kernel32.SetConsoleTextAttribute(std_out_handle, texto[1])
-        stdout.write(texto)
-        print()
+    print(f'{texto}')
     # cor padrão é 7, white
     windll.kernel32.SetConsoleTextAttribute(std_out_handle, 7)
 
