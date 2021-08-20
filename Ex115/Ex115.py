63e9cd746e72413e2ae9508d027f52f15b5a5c43
Fernando <fernando22future@gmail.com>
2021-08-19 21:08:51 -0300

    Atualização do menu

--- Ex115/Ex115.py
+++ Ex115/Ex115.py
@@ -2,27 +2,36 @@
 
 from cadastropessoas import cadastrar, listar
 from ctypes import windll
+from time import sleep
 
-menu = 'MENU DE OPÇÕES'
-std_out_handle = windll.kernel32.GetStdHandle(-11)
 
-while True:
-    windll.kernel32.SetConsoleTextAttribute(std_out_handle, 7)
-    print('--' * len(menu))
-    print(' ' * ((len(menu)//2)-1), menu)
-    print('--' * len(menu))
+def menuopcoes():
+    windll.kernel32.SetConsoleTextAttribute(std_out_handle, 1)
+    print('--' * menu)
+    print(menutxt.center(80))
+    print('--' * menu)
     print(' '*3, '1 - Cadastrar pessoa')
     print(' '*3, '2 - Listar cadastro')
     print(' '*3, '3 - Sair')
-    print('--' * len(menu))
+    print('--' * menu)
+
+
+menu = 40
+menutxt = 'Menu de opções'
+std_out_handle = windll.kernel32.GetStdHandle(-11)
+
+while True:
+    menuopcoes()
     while True:
         try:
-            windll.kernel32.SetConsoleTextAttribute(std_out_handle, 7)
+            windll.kernel32.SetConsoleTextAttribute(std_out_handle, 3)
             opcao = int(input('Qual a opção desejada? '))
             break
         except:
             windll.kernel32.SetConsoleTextAttribute(std_out_handle, 4)
             print('Digite um número inteiro')
+            sleep(1)
+            menuopcoes()
     if opcao == 1:
         cadastrar.cadastro()
     if opcao == 2:
@@ -35,3 +44,4 @@ while True:
     if opcao < 1 or opcao > 3:
         windll.kernel32.SetConsoleTextAttribute(std_out_handle, 4)
         print('Escolha um número entre 1 e 3')
+    sleep(1)
