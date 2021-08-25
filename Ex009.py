04fe641220f36df85a754b2944d60f245cf6cabd
Fernando <fernando22future@gmail.com>
2021-08-25 19:09:06 -0300

    Atualização

--- Ex009.py
+++ Ex009.py
@@ -1,20 +1,16 @@
 n = int(input('Digite um número para calcular a tabuada '))
 m = 1
 
-# print('{} x {:2} = {}'.format(n, 1, n*1))
-# print('{} x {:2} = {}'.format(n, 2, n*2))
-# print('{} x {:2} = {}'.format(n, 3, n*3))
-# print('{} x {:2} = {}'.format(n, 4, n*4))
-# print('{} x {:2} = {}'.format(n, 5, n*5))
-# print('{} x {:2} = {}'.format(n, 6, n*6))
-# print('{} x {:2} = {}'.format(n, 7, n*7))
-# print('{} x {:2} = {}'.format(n, 8, n*8))
-# print('{} x {:2} = {}'.format(n, 9, n*9))
-# print('{} x {:2} = {}'.format(n, 10, n*10))
-
-while m <= 10:
-    print('{} x {:2} = {}'.format(n, m, n*m))
-    m += 1
+print('{} x {:2} = {}'.format(n, 1, n*1))
+print('{} x {:2} = {}'.format(n, 2, n*2))
+print('{} x {:2} = {}'.format(n, 3, n*3))
+print('{} x {:2} = {}'.format(n, 4, n*4))
+print('{} x {:2} = {}'.format(n, 5, n*5))
+print('{} x {:2} = {}'.format(n, 6, n*6))
+print('{} x {:2} = {}'.format(n, 7, n*7))
+print('{} x {:2} = {}'.format(n, 8, n*8))
+print('{} x {:2} = {}'.format(n, 9, n*9))
+print('{} x {:2} = {}'.format(n, 10, n*10))
 
 # Quando se usa :2 entre colchetes, o conteúdo vai ficar alinhado a direita com duas casas como é o caso dos números.
 # Não confundir com :.2f que serve para limitar a quantidade de casas decimais que aparecem depois da virgula.
--- Ex099.py
+++ Ex099.py
@@ -1,4 +1,4 @@
-"""Faça um programa que tenha uma função chamada maior(), que receba vários parãmetros com valores inteiros. Seu programa deve anlisar todos os valores e dizer qual deles é o maior. Informar a quantidade de números passados como parâmetros."""
+"""Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa deve anlisar todos os valores e dizer qual deles é o maior. Informar a quantidade de números passados como parâmetros."""
 
 def maior(* v):
     print()
